7ce5f9cd890729b78397268319d51133153e49c1
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-04-16 12:08:02 +0200

    adding errors tab to page

--- dash_interface/app.py
+++ dash_interface/app.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import sys
+
 sys.path.append("../")
 
 from dash.dependencies import Input, Output, State
@@ -10,6 +11,7 @@ import run.process_file as pf
 from dash_interface.helper import run_standalone_app, sent_tokenizer, word_tokenizer, tagger
 
 DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
 
 def header_colors():
     return {
@@ -23,13 +25,11 @@ def description():
 
 
 def layout():
-
     """
     Returns page layout. Modifications of page structure should be passed here
     :return:  Layout
     """
-
-    return html.Div(id='seq-view-body', className='app-body', children=[
+    div = html.Div(id='seq-view-body', className='app-body', children=[
         html.Div(id='seq-view-control-tabs',
                  className="four columns div-user-controls",
                  children=[
@@ -81,255 +81,42 @@ def layout():
                                      ]
                                  ),
 
-
                              ])
                          ),
-                         # dcc.Tab(
-                         #     label='Options',
-                         #     value='sequence',
-                         #     children=html.Div(className='control-tab', children=[
-                         #         html.Div(
-                         #             id='seq-view-entry-dropdown-container',
-                         #             className='app-controls-block',
-                         #             children=[
-                         #                 html.Div(
-                         #                     "View entry:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.Dropdown(
-                         #                     className='app-dropdown',
-                         #                     id='fasta-entry-dropdown',
-                         #                     options=[
-                         #                         {'label': 1, 'value': 0}
-                         #                     ],
-                         #                     value=0
-                         #                 ),
-                         #                 html.Div(
-                         #                     id='seq-view-number-entries',
-                         #                     className='app-controls-desc'
-                         #                 )
-                         #             ]
-                         #         ),
-                         #         html.Br(),
-                         #         html.Div(
-                         #             id='seq-view-sel-or-cov-container',
-                         #             children=[
-                         #                 html.Div(
-                         #                     "Selection or coverage:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.RadioItems(
-                         #                     id='selection-or-coverage',
-                         #                     options=[
-                         #                         {
-                         #                             'label': 'selection',
-                         #                             'value': 'sel'
-                         #                         },
-                         #                         {
-                         #                             'label': 'coverage',
-                         #                             'value': 'cov'
-                         #                         }
-                         #                     ],
-                         #                     value='sel'
-                         #                 )
-                         #             ]
-                         #         ),
-                         #
-                         #         html.Hr(),
-                         #
-                         #         html.Div(id='cov-options', children=[
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     "Add coverage from selection by:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.RadioItems(
-                         #                     id='mouse-sel-or-subpart-sel',
-                         #                     options=[
-                         #                         {'label': 'mouse',
-                         #                          'value': 'mouse'},
-                         #                         {'label': 'search',
-                         #                          'value': 'subpart'}
-                         #                     ],
-                         #                     value='mouse'
-                         #                 ),
-                         #             ]),
-                         #
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     "Text color:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.Input(
-                         #                     id='coverage-color',
-                         #                     type='text',
-                         #                     value='rgb(255, 0, 0)'
-                         #                 )
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     "Background color:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.Input(
-                         #                     id='coverage-bg-color',
-                         #                     type='text',
-                         #                     value='rgb(0, 0, 255)'
-                         #                 ),
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     "Tooltip:",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.Input(
-                         #                     id='coverage-tooltip',
-                         #                     type='text',
-                         #                     value='',
-                         #                     placeholder='hover text'
-                         #                 ),
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     "Underscore text: ",
-                         #                     className='app-controls-name'
-                         #                 ),
-                         #                 dcc.Checklist(
-                         #                     id='coverage-underscore',
-                         #                     options=[
-                         #                         {'label': '',
-                         #                          'value': 'underscore'}
-                         #                     ],
-                         #                     value=[]
-                         #                 )
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Button(
-                         #                     id='coverage-submit',
-                         #                     children='Submit'
-                         #                 ),
-                         #                 html.Button(
-                         #                     id='coverage-reset',
-                         #                     children='Reset'
-                         #                 )
-                         #             ])
-                         #         ]),
-                         #
-                         #         html.Div(id='seq-view-sel-slider-container', children=[
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 html.Div(
-                         #                     className='app-controls-name',
-                         #                     children="Selection region:"
-                         #                 ),
-                         #                 dcc.RadioItems(
-                         #                     id='sel-slider-or-input',
-                         #                     options=[
-                         #                         {'label': 'slider', 'value': 'slider'},
-                         #                         {'label': 'input', 'value': 'input'}
-                         #                     ],
-                         #                     value='slider'
-                         #                 )
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 dcc.RangeSlider(
-                         #                     id='sel-slider',
-                         #                     min=0,
-                         #                     max=0,
-                         #                     step=1,
-                         #                     value=[0, 0]
-                         #                 )
-                         #             ]),
-                         #             html.Div(className='app-controls-block', children=[
-                         #                 # optional numeric input for longer sequences
-                         #                 html.Div(
-                         #                     id='sel-region-inputs',
-                         #                     children=[
-                         #                         "From: ",
-                         #                         dcc.Input(
-                         #                             id='sel-region-low',
-                         #                             type='number',
-                         #                             min=0,
-                         #                             max=0,
-                         #                             placeholder="low"
-                         #                         ),
-                         #                         "To: ",
-                         #                         dcc.Input(
-                         #                             id='sel-region-high',
-                         #                             type='number',
-                         #                             min=0,
-                         #                             max=0,
-                         #                             placeholder="high"
-                         #                         ),
-                         #                     ],
-                         #                     style={'display': 'none'}
-                         #                 )
-                         #             ]),
-                         #
-                         #             html.Div(
-                         #                 id='seq-view-dna-or-protein-container',
-                         #                 children=[
-                         #                     html.Div(className='app-controls-block', children=[
-                         #                         html.Div(
-                         #                             className='app-controls-name',
-                         #                             children="Translate selection from:"
-                         #                         ),
-                         #                         dcc.Dropdown(
-                         #                             id='translation-alphabet',
-                         #                             options=[
-                         #                                 {'label': 'DNA',
-                         #                                  'value': 'dna'},
-                         #                                 {'label': 'RNA',
-                         #                                  'value': 'rna'}
-                         #                             ],
-                         #                             value=None
-                         #                         )
-                         #                     ])
-                         #                 ]
-                         #             ),
-                         #
-                         #             html.Div(
-                         #                 className='app-controls-name',
-                         #                 children="Selection highlight color:"
-                         #             ),
-                         #             dcc.Dropdown(
-                         #                 className='app-dropdown',
-                         #                 id='sel-color',
-                         #                 options=[
-                         #                     {'label': 'violet', 'value': 'violet'},
-                         #                     {'label': 'indigo', 'value': 'indigo'},
-                         #                     {'label': 'blue', 'value': 'blue'},
-                         #                     {'label': 'green', 'value': 'green'},
-                         #                     {'label': 'yellow', 'value': 'yellow'},
-                         #                     {'label': 'orange', 'value': 'orange'},
-                         #                     {'label': 'red', 'value': 'red'}
-                         #                 ],
-                         #                 value='indigo'
-                         #             )
-                         #         ])
-                         #     ])
-                         # )
+                         dcc.Tab(
+                             label='Errors',
+                             value='errors',
+                             children=html.Div(className='page', children=[
+                                 html.H4(className='what-is', children="Visualisation et correction d'erreures"),
+                                 html.P("Mais combien des données j'en ai besoin pour entrainer un modele similaire ?"
+                                        "Ici on vous montre les differences par rapport a la taille d"),
+                                 html.Br(),
+                                 html.P("Dans l'onglet données vous pouvez charger un fichier afin de le faire "
+                                        "pseudonimizer par l'algorithme "),
+
+                             ])
+                         )
                      ]),
                  ]),
         html.Div(id='seq-view-control',
                  className="seven columns div-user-controls",
-                 children=html.Div(className='page',id="text-output")),
+                 children=html.Div(className='page', id="text-output")),
     ])
+    return div
 
 
 def callbacks(_app):
     """ Define callbacks to be executed on page change"""
 
-
-
-    @_app.callback(Output('text-output', 'children'),
-                  [Input('upload-data', 'contents'),
-                  Input('upload-data', 'filename'),
-                   Input('upload-data', 'last_modified')])
-    def update_sequence(content, list_of_names, list_of_dates):
-
+    @_app.callback(Output('text-output', 'chil  dren'),
+                   [Input('upload-data', 'contents'),
+                    Input('upload-data', 'filename'),
+                    Input('upload-data', 'last_modified')])
+    def update_sequence(content, list_of_file_names, list_of_dates):
         if content == None:
-
-            return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici", style = {"width": "100%", "display": "flex", "align-items": "center", "justify-content": "center"})
+            return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici",
+                            style={"width": "100%", "display": "flex", "align-items": "center",
+                                   "justify-content": "center"})
 
         content_type, content_string = content.split(',')
         decoded = base64.b64decode(content_string)
@@ -343,12 +130,10 @@ def callbacks(_app):
         return pseudo
 
 
-
-
 # only declare app/server if the file is being run directly
 if 'DEMO_STANDALONE' not in os.environ:
     app = run_standalone_app(layout, callbacks, header_colors, __file__)
     server = app.server
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
+    app.run_server(debug=False, port=8050)
--- dash_interface/helper.py
+++ dash_interface/helper.py
@@ -8,42 +8,8 @@ from nltk.tokenize import WordPunctTokenizer, PunktSentenceTokenizer
 from flair.models import SequenceTagger
 
 word_tokenizer = WordPunctTokenizer()
-tagger = SequenceTagger.load('fr-ner')
+tagger = SequenceTagger.load('/home/pavel/code/pseudo_conseil_etat/models/nb_docs_datasets/1600_200_200/best-model.pt')
 sent_tokenizer = PunktSentenceTokenizer("nltk_data/tokenizers/punkt/french.pickle")
-
-
-def run_standalone_app(
-        layout,
-        callbacks,
-        header_colors,
-        filename
-):
-    """Run demo app (tests/dashbio_demos/*/app.py) as standalone app."""
-    app = dash.Dash(__name__)
-    app.scripts.config.serve_locally = True
-    # Handle callback to component with id "fullband-switch"
-    app.config['suppress_callback_exceptions'] = True
-
-    # Get all information from filename
-
-    app_name = "Etalab Pseudonymisation"
-
-    app_title = "{}".format(app_name.replace('-', ' ').title())
-
-    # Assign layout
-    app.layout = app_page_layout(
-        page_layout=layout(),
-        app_title=app_title,
-        app_name=app_name,
-        standalone=True,
-        **header_colors()
-    )
-
-    # Register all callbacks
-    callbacks(app)
-
-    # return app object
-    return app
 
 
 def app_page_layout(page_layout,
@@ -106,3 +72,37 @@ def app_page_layout(page_layout,
             )
         ],
     )
+
+
+def run_standalone_app(
+        layout,
+        callbacks,
+        header_colors,
+        filename
+):
+    """Run demo app (tests/dashbio_demos/*/app.py) as standalone app."""
+    app = dash.Dash(__name__)
+    app.scripts.config.serve_locally = True
+    # Handle callback to component with id "fullband-switch"
+    app.config['suppress_callback_exceptions'] = True
+
+    # Get all information from filename
+
+    app_name = "Etalab Pseudonymisation"
+
+    app_title = "{}".format(app_name.replace('-', ' ').title())
+
+    # Assign layout
+    app.layout = app_page_layout(
+        page_layout=layout(),
+        app_title=app_title,
+        app_name=app_name,
+        standalone=True,
+        **header_colors()
+    )
+
+    # Register all callbacks
+    callbacks(app)
+
+    # return app object
+    return app
