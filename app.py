a92ebda60debed7bb273ce43357276d8d6f180ad
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-04-22 15:06:50 +0200

    refactored tabs in main div

--- app.py
+++ app.py
@@ -28,6 +28,72 @@ def description():
     return "Test d'une application de pseudonimysation pour le Lab IA"
 
 
+tab_about_content = dcc.Tab(
+    label='À propos',
+    value='what-is',
+    children=html.Div(className='page', children=[
+        html.H4(className='what-is', children='Pourquoi un outil de pseudonymization?'),
+        html.P("Le règlement général sur la protection des données (RGPD) n’impose pas aux "
+               "administrations d’anonymiser les documents qu’elles détiennent. Ainsi "
+               "lorsque les "
+               "documents administratifs comportent des données personnelles, ils ne peuvent "
+               "être rendus publics qu'après avoir fait l'objet d'un traitement permettant "
+               "de rendre impossible l'identification de ces personnes."),
+        html.Br(),
+        html.P("Dans l'onglet données vous pouvez charger un fichier afin de le faire "
+               "pseudonimizer par l'algorithme "),
+
+    ])
+)
+
+tab_upload_content = dcc.Tab(
+    label='Données',
+    value='data',
+    children=html.Div(className='control-tab', children=[
+        html.Div(
+            id='preloaded-and-uploaded-alert',
+            className='app-controls-desc',
+            children=[
+                'You have uploaded your own data. In order \
+                to view it, please ensure that the "preloaded \
+                sequences" dropdown has been cleared.'
+            ],
+            style={'display': 'none'}
+        ),
+        html.Div("Veuiller choisir un fichier",
+                 className='app-controls-block'),
+
+        html.Div(
+            id='seq-view-fast-upload',
+            children=[
+                dcc.Upload(
+                    id='upload-data',
+                    className='control-upload',
+                    children=html.Div([
+                        "Faire glisser ou cliquer pour charger un fichier"
+                    ]),
+                ),
+            ]
+        ),
+
+    ])
+)
+
+tab_errors_content = dcc.Tab(
+    label='Errors',
+    value='errors',
+    children=html.Div(className='page', children=[
+        html.H4(className='what-is', children="Visualisation et correction d'erreures"),
+        html.P("Mais combien des données j'en ai besoin pour entrainer un modele similaire ?"
+               "Ici on vous montre les differences par rapport a la taille d"),
+        html.Br(),
+        html.P("Dans l'onglet données vous pouvez charger un fichier afin de le faire "
+               "pseudonimizer par l'algorithme "),
+
+    ])
+)
+
+
 def layout():
     """
     Returns page layout. Modifications of page structure should be passed here
@@ -38,92 +104,20 @@ def layout():
                  className="four columns div-user-controls",
                  children=[
                      dcc.Tabs(id='seq-view-tabs', value='what-is', children=[
-                         dcc.Tab(
-                             label='À propos',
-                             value='what-is',
-                             children=html.Div(className='page', children=[
-                                 html.H4(className='what-is', children='Pourquoi un outil de pseudonymization?'),
-                                 html.P("Le règlement général sur la protection des données (RGPD) n’impose pas aux "
-                                        "administrations d’anonymiser les documents qu’elles détiennent. Ainsi "
-                                        "lorsque les "
-                                        "documents administratifs comportent des données personnelles, ils ne peuvent "
-                                        "être rendus publics qu'après avoir fait l'objet d'un traitement permettant "
-                                        "de rendre impossible l'identification de ces personnes."),
-                                 html.Br(),
-                                 html.P("Dans l'onglet données vous pouvez charger un fichier afin de le faire "
-                                        "pseudonimizer par l'algorithme "),
-
-                             ])
-                         ),
-                         dcc.Tab(
-                             label='Données',
-                             value='data',
-                             children=html.Div(className='control-tab', children=[
-                                 html.Div(
-                                     id='preloaded-and-uploaded-alert',
-                                     className='app-controls-desc',
-                                     children=[
-                                         'You have uploaded your own data. In order \
-                                         to view it, please ensure that the "preloaded \
-                                         sequences" dropdown has been cleared.'
-                                     ],
-                                     style={'display': 'none'}
-                                 ),
-                                 html.Div("Veuiller choisir un fichier",
-                                          className='app-controls-block'),
-
-                                 html.Div(
-                                     id='seq-view-fast-upload',
-                                     children=[
-                                         dcc.Upload(
-                                             id='upload-data',
-                                             className='control-upload',
-                                             children=html.Div([
-                                                 "Faire glisser ou cliquer pour charger un fichier"
-                                             ]),
-                                         ),
-                                     ]
-                                 ),
-
-                             ])
-                         ),
-                         dcc.Tab(
-                             label='Errors',
-                             value='errors',
-                             children=html.Div(className='page', children=[
-                                 html.H4(className='what-is', children="Visualisation et correction d'erreures"),
-                                 html.P("Mais combien des données j'en ai besoin pour entrainer un modele similaire ?"
-                                        "Ici on vous montre les differences par rapport a la taille d"),
-                                 html.Br(),
-                                 html.P("Dans l'onglet données vous pouvez charger un fichier afin de le faire "
-                                        "pseudonimizer par l'algorithme "),
-
-                             ])
-                         )
+                         tab_about_content,
+                         tab_upload_content,
+                         tab_errors_content
                      ]),
                  ]),
-        html.Div(id='seq-view-control',
-                 className="seven columns div-user-controls",
-                 children=dbc.Container(
-                     [
-                         dbc.Row([dbc.Col(html.Div(id="text-output-tagged", style={"maxHeight": "400px",
-                                                                                   "overflow-y": "scroll"}))],
-                                 className="h-50"),
-                         dbc.Row([dbc.Col(html.Div(id="text-output-anonym", style={"maxHeight": "400px",
-                                                                                   "overflow-y":"scroll"}))],
-                                 className="h-50"),
-                 ], style={"height": "100vh"}, className="page"))
+        html.Div(id='seq-view-control', className="seven columns")
     ])
     return div
-    # html.Div(id="text-output-tagged"),
-    # html.Br(),
-    # html.Div(id="text-output-anonym")
+
 
 def callbacks(_app):
     """ Define callbacks to be executed on page change"""
 
-    @_app.callback([Output('text-output-tagged', 'children'),
-                    Output('text-output-anonym', 'children')],
+    @_app.callback(Output('seq-view-control', 'children'),
                    [Input('upload-data', 'contents'),
                     Input('upload-data', 'filename'),
                     Input('upload-data', 'last_modified')])
@@ -131,7 +125,7 @@ def callbacks(_app):
         if content == None:
             return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici",
                             style={"width": "100%", "display": "flex", "align-items": "center",
-                                   "justify-content": "center"}), None
+                                   "justify-content": "center"})
         extension = list_of_file_names.split(".")[-1]
         temp_path = f"/tmp/output.{extension}"
         content_type, content_string = content.split(',')
@@ -144,7 +138,21 @@ def callbacks(_app):
 
         html_pseudoynmized, html_tagged = dash_interface.helper.create_html_outputs(text=decoded, tagger=tagger)
 
-        return html_tagged, html_pseudoynmized
+        children = dbc.Container(
+            [
+                html.H4("Document annotée"),
+                dbc.Row([dbc.Col(html.Div(children=html_tagged, id="text-output-tagged", style={"maxHeight": "500px",
+                                                                                                "overflow-y": "scroll"}))],
+                        className="h-50", style={"margin-bottom": "2cm"}),
+
+                html.H4("Document pseudonymisé"),
+                dbc.Row([dbc.Col(html.Div(children=html_pseudoynmized,
+                                          id="text-output-anonym", style={"maxHeight": "500px",
+                                                                          "overflow-y": "scroll"}))],
+                        className="h-50"),
+            ], style={"height": "100vh"}, className="page")
+
+        return children
 
 
 # only declare app/server if the file is being run directly
