61c26e682106879d6d6da7b4faeaf4a59b60d7a0
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-05-01 19:08:55 +0200

    Reworked callback logic of upload tab. Added better style to the tabs cards

--- components/tab_upload.py
+++ components/tab_upload.py
@@ -8,11 +8,10 @@ from flair.models import SequenceTagger
 from dash_interface.data_ETL import load_text, create_upload_tab_html_output
 
 TEXTE_EXEMPLE = """
-        Lecture du mercredi 28 février 2018
+Lecture du mercredi 28 février 2018
 REPUBLIQUE FRANCAISE
 
 AU NOM DU PEUPLE FRANCAIS
-
 
 Vu la procédure suivante :
 
@@ -33,7 +32,7 @@ l'enseignement supérieur, de la recherche et de l'innovation demande au Conseil
 tagger = SequenceTagger.load('/home/pavel/code/pseudo_conseil_etat/models/flair_embeds/1600_200_200/best-model.pt')
 
 tab_upload_content = dbc.Tab(
-    label='Données',
+    label='Pseudonymise un document',
     tab_id="tab-upload",
     children=html.Div(className='control-tab', children=[
         html.Div("Veuillez choisir un fichier à analyser (type .doc, .docx, .txt. Max 200 Ko)",
@@ -49,7 +48,7 @@ tab_upload_content = dbc.Tab(
                 ]),
             ),
         ),
-        html.Div(["Ou ", html.B("lancez le texte exemple", id="exemple-text")],
+        html.Div(["Ou ", html.B("lancez le texte exemple en cliquant ici", id="exemple-text")],
                  className='app-controls-block'),
 
     ])
@@ -57,11 +56,11 @@ tab_upload_content = dbc.Tab(
 
 
 def pane_upload_content(contents, file_name, n_clicks, data):
-    if contents is None and n_clicks is None:
-        data = data or {"n_clicks": 0}
-        return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici",
-                        style={"width": "100%", "display": "flex", "align-items": "center",
-                               "justify-content": "center"}), data
+    # if contents is None and n_clicks is None:
+    #     data = data or {"n_clicks": 0}
+    #     return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici",
+    #                     style={"width": "100%", "display": "flex", "align-items": "center",
+    #                            "justify-content": "center"}), data
     if n_clicks is not None and n_clicks > data["n_clicks"]:
         decoded = TEXTE_EXEMPLE
         content_id = md5(decoded.encode("utf-8")).hexdigest()
@@ -70,7 +69,7 @@ def pane_upload_content(contents, file_name, n_clicks, data):
         if content_id in data and data[content_id]:
             children = data[content_id]
             return children, data
-    else:
+    elif contents:
         file_name, extension = file_name.split(".")
         temp_path = f"/tmp/output.{extension}"
         content_type, content_string = contents.split(',')
@@ -89,14 +88,27 @@ def pane_upload_content(contents, file_name, n_clicks, data):
         f.write(decoded)
         f.close()
         decoded = load_text(temp_path)
+    else:
+        data = data or {"n_clicks": 0}
+        return html.Div("Chargez un fichier dans l'onglet données pour le faire apparaitre pseudonymisé ici",
+                        style={"width": "100%", "display": "flex", "align-items": "center",
+                               "justify-content": "center"}), data
 
     html_pseudoynmized, html_tagged = create_upload_tab_html_output(text=decoded, tagger=tagger)
 
-    pseudo_content = dbc.Card(dbc.CardBody(html_pseudoynmized), className="mt-3",
-                            style={"maxHeight": "750px", "overflow-y": "scroll"})
+    pseudo_content = dbc.Card(dbc.CardBody(html_pseudoynmized),
+                            style={"maxHeight": "750px", "overflow-y": "scroll",
+                                   "background-color": "transparent",
+                                   "font-family": 'Arial',
+                                   "border": "none"},
+                              )
 
-    tagged_content = dbc.Card(dbc.CardBody(html_tagged), className="mt-3",
-                            style={"maxHeight": "750px", "overflow-y": "scroll"})
+    tagged_content = dbc.Card(dbc.CardBody(html_tagged),
+                            style={"maxHeight": "750px", "overflow-y": "scroll",
+                                   "font-family": 'Arial',
+                                   "background-color": "transparent",
+                                   "border": "none"},
+                              )
 
     children = dbc.Tabs(
         [
@@ -105,7 +117,5 @@ def pane_upload_content(contents, file_name, n_clicks, data):
         ]
     )
 
-    # data.clear()
     data = {"n_clicks": data["n_clicks"], content_id: children}
-    # data = {"n_clicks": n_clicks}
     return children, data
